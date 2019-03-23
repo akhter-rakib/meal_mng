3b0198e26bf59c4dbc000f390ce2db612333bf5a
rakibccj <rakibccj@gmail.com>
2019-03-23 14:07:19 +0600

    initiate project

---
+++ src/main/java/com/vaibrothers/meal_mng/MealMngApplication.java
@@ -0,0 +1,13 @@
+package com.vaibrothers.meal_mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealMngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vaibrothers/meal_mng/MealMngApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vaibrothers.meal_mng;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MealMngApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
