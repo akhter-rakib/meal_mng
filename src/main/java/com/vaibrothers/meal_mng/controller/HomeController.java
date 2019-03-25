a2bc280732fc02f522233be58fc16210567c60de
rakibccj <rakibccj@gmail.com>
2019-03-25 17:25:27 +0600

    login/logout added

--- src/main/java/com/vaibrothers/meal_mng/controller/HomeController.java
+++ src/main/java/com/vaibrothers/meal_mng/controller/HomeController.java
@@ -1,14 +1,42 @@
 package com.vaibrothers.meal_mng.controller;
 
 import com.vaibrothers.meal_mng.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @GetMapping(value = "/home")
     public String home() {
         return "home";
+    }
+
+    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registration() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/admin/home", method = RequestMethod.GET)
+    public void homeAdmin() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth.getName());
+        System.out.println("Test");
     }
 }
