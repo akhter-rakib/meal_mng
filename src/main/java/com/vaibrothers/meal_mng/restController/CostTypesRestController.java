253e643b6aadedf1fa50f7f6a38b0b122fcf0a2f
Atuar Rahman <atuar.avis@gmail.com>
2019-04-05 20:06:42 +0600

    added Costing Types name view

--- src/main/java/com/vaibrothers/meal_mng/restController/CostTypesRestController.java
+++ src/main/java/com/vaibrothers/meal_mng/restController/CostTypesRestController.java
@@ -7,11 +7,13 @@ import com.vaibrothers.meal_mng.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class CostTypesRestController {
@@ -21,6 +23,11 @@ public class CostTypesRestController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping(value = "/getCostTypes")
+    public List<CostTypes> getCostTypes() {
+        return costTypesService.list();
+    }
 
     @PostMapping(value = "/saveCostingTypes")
     public void saveCostTypes(@RequestBody CostTypes costTypes){
--- src/main/resources/static/app/costTypes.js
+++ src/main/resources/static/app/costTypes.js
@@ -1,11 +1,21 @@
 var app = angular.module('costingTypesEntry', []);
 app.controller('CostingTypesController', ['$http', '$scope', function ($http, $scope) {
     var costingTypesEntry = this;
+    costingTypesEntry.costingTypesList = [];
     costingTypesEntry.costingTypesModel = {
         id: '',
         costingName: ''
     };
+    costingTypesList();
 
+    function costingTypesList() {
+        $http.get('/getCostTypes').success(function (d) {
+            console.log(d);
+            costingTypesEntry.costingTypesList = d;
+        }).error(function (data, status, headers) {
+            console.log(data);
+        });
+    };
 
     costingTypesEntry.saveCostingTypes = function (costingTypes) {
         console.log(costingTypes);
@@ -13,6 +23,7 @@ app.controller('CostingTypesController', ['$http', '$scope', function ($http, $s
             /* self.success = 'Unit name successfully added in system !!!!';
              self.error = null;*/
             //getUserList();
+            costingTypesList();
             reset();
         }).error(function (data, status, headers) {
             console.log(status);
